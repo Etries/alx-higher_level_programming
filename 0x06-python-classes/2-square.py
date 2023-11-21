872dba9678e3193f7935be27cf3af6b80e49c4d4
Elyud <elyudmichael@gmail.com>
2023-11-21 12:02:56 +0000

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" An square class"""
+
+
+class Square:
+    """ square class with attributes"""
+
+    def __init__(self, size=0):
+        """ init function """
+        if not size.isdigit:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = size
