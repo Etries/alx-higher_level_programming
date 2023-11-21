a8c33bf13f435f6fe4741dd67fd097489d2bdb73
Elyud <elyudmichael@gmail.com>
2023-11-21 12:27:14 +0000

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -21,7 +21,7 @@ class Square:
         elif value < 0:
             raise ValueError('size must be >= 0')
         else:
-            self.__size = value 
+            self.__size = value
 
     def area(self):
         """returns the area"""
