0147cc240d5d4dda5ea0bb93067e19ec01122cdd
Elyud <elyudmichael@gmail.com>
2023-11-21 11:55:24 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" An square class"""
+
+
+class Square:
+    """ square class with attributes"""
+    _size = 0
+    
+    def __init__(self, size):
+        self._size = size
+    
